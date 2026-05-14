87453183b4135f10909d616d76e5b9657683aeec
badinkajink <williamxie00@gmail.com>
2026-05-14 10:32:19 -0600

    stepper driver code

--- src/morphohand/driver/hand/src/main.cpp
+++ src/morphohand/driver/hand/src/main.cpp
@@ -6,12 +6,12 @@ AccelStepper motor1(AccelStepper::FULL4WIRE, 8, 10, 9, 11);
 AccelStepper motor2(AccelStepper::FULL4WIRE, 4, 6,  5, 7);
 
 void setup() {
-  motor1.setMaxSpeed(200);
-  motor1.setAcceleration(100);
+  motor1.setMaxSpeed(100);
+  motor1.setAcceleration(10);
   motor1.moveTo(400);   // 2 revolutions
 
-  motor2.setMaxSpeed(200);
-  motor2.setAcceleration(100);
+  motor2.setMaxSpeed(100);
+  motor2.setAcceleration(10);
   motor2.moveTo(-400);  // 2 revolutions opposite direction
 }
 
